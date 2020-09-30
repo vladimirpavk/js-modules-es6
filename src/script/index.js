cf318e47b99fe74bddcb2f65cad23ef6bd5c0487
Vladimir Pavković <vladimirpavk@gmail.com>
2020-09-30 10:29:58 +0200

    +

--- src/script/index.js
+++ src/script/index.js
@@ -4,7 +4,7 @@ import * as HtmlParser from './utils/htmlParser.js';
 
 //console.log('From module imports', Counter, Math, HtmlParser);
 //let counter = new Counter();
-let counterElement = HtmlParser.getCounter();
+/* let counterElement = HtmlParser.getCounter();
 
 setInterval(()=>{
     counterElement.innerHTML = counter.getValue();
@@ -25,4 +25,49 @@ HtmlParser.getMinusButton().addEventListener('click', (event)=>{
 
 HtmlParser.getNextPageButton().addEventListener('click', (event)=>{
     HtmlParser.getWindow().location='page2.html';
-})
+}) */
+
+let bodyRoot = document.getElementById('root');
+
+const resetButton = document.createElement('button');
+/* resetButton.id = 'resetButton'; */
+resetButton.setAttribute('id', 'resetButton');
+resetButton.innerHTML = 'Reset';
+
+const plusButton = document.createElement('button');
+/* plusButton.id = 'plusButton'; */
+plusButton.setAttribute('id', 'plusButton');
+plusButton.innerHTML = '+5';
+
+const minusButton = document.createElement('button');
+/* minusButton.id = 'minusButton'; */
+minusButton.setAttribute('id', 'minusButton');
+minusButton.innerHTML = '-5';
+
+const buttonContainer = document.createElement('div');
+buttonContainer.classList.add('buttonContainer');
+buttonContainer.appendChild(resetButton);
+buttonContainer.appendChild(plusButton);
+buttonContainer.appendChild(minusButton);
+
+const labelCounter = document.createElement('label');
+labelCounter.classList.add('counter');
+
+const labelCounterLabel = document.createElement('label')
+labelCounterLabel.classList.add('counterLabel');
+labelCounterLabel.innerHTML = 'js-module counter';
+
+const nextPageButton = document.createElement('button');
+nextPageButton.classList.add('nextPageButton');
+nextPageButton.innerHTML = 'Next Page';
+
+const containerDiv = document.createElement('div');
+containerDiv.classList.add('container');
+containerDiv.appendChild(labelCounterLabel);
+containerDiv.appendChild(labelCounter);
+containerDiv.appendChild(buttonContainer);
+containerDiv.appendChild(nextPageButton);
+
+bodyRoot.appendChild(containerDiv);
+
+console.log(containerDiv);
--- src/script/utils/counter.js
+++ src/script/utils/counter.js
@@ -2,8 +2,14 @@ class Counter{
     constructor(){
         this.value = 0;
     }
-    getValue = ()=>this.value;
-    increment = ()=>this.value = this.value + 1;
+   /*  getValue = ()=>this.value;
+    increment = ()=>this.value = this.value + 1; */
+    getValue(){
+        return this.value;
+    }
+    increment(){
+        return this.value = this.value + 1;
+    }
 }
 
 export default Counter;
